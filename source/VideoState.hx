f3776863bea728ae20ce1547609195ae842f1b94
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-04-09 05:33:37 -0300

    change

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -1,60 +1,22 @@
-package;
-
-import flixel.FlxCamera;
-import Achievements;
-import flixel.text.FlxText;
-import flixel.FlxState;
+#if mobileC
+import extension.webview.WebView;
+#end
+import flixel.FlxBasic;
 import flixel.FlxG;
 
-import extension.webview.WebView;
+class VideoState extends FlxBasic {
+        public var finishCallback:Void->Void = null;
 
-using StringTools;
-
-class VideoState extends MusicBeatState
-{
-	public static var androidPath:String = 'file:///android_asset/';
-	public var nextState:FlxState;
-	private var camAchievement:FlxCamera;
-
-	var someshit:FlxText;
-
-	public function new(source:String, toTrans:FlxState)
-	{
+	public function new(name:String) {
 		super();
 
-		camAchievement = new FlxCamera();
-		camAchievement.bgColor.alpha = 0;
-	
-		FlxG.cameras.add(camAchievement);
-
-		someshit = new FlxText(0, 0, 0, "Video Ended\nTap To Screen", 48);
-		someshit.screenCenter();
-		someshit.alpha = 0;
-		add(someshit);
-
-		nextState = toTrans;
-
-		WebView.onClose=onClose;
-		WebView.onURLChanging=onURLChanging;
-
-		WebView.open(androidPath + source + '.html', false, null, ['http://exitme(.*)']);
-	}
-
-	public override function update(dt:Float) {
-		for (touch in FlxG.touches.list)
-			if (touch.justReleased)
-				onClose();
-
-		super.update(dt);	
-	}
-
-	function onClose(){
-		someshit.alpha = 0;
-		MusicBeatState.switchState(nextState);
-	}
-
-	function onURLChanging(url:String) {
-		someshit.alpha = 1;
-		if (url == 'http://exitme(.*)') onClose();
+	        #if mobileC
+                WebView.playVideo('file:///android_asset/' + name, true);
+                WebView.onComplete = function(){
+		        if (finishCallback != null){
+			        finishCallback();
+		        }
+                }
+		#end
 	}
 }
